c210aeb85da21f02eec551760e1ef3abb8870f47
AbnerAstete <abnerastete2001@gmail.com>
2024-03-27 21:50:32 +0100

    Clean ESDR data

--- airflow/dags/air_polution.py
+++ airflow/dags/air_polution.py
@@ -9,7 +9,19 @@ from airflow.operators.postgres_operator import PostgresOperator
 
 from helpers import (
     extract_esdr,
-    extract_smell
+    extract_smell,
+    clean_esdr_1,
+    clean_esdr_3,
+    clean_esdr_23,
+    clean_esdr_24,
+    clean_esdr_26,
+    clean_esdr_27,
+    clean_esdr_28,
+    clean_esdr_29,
+    clean_esdr_43,
+    clean_esdr_3506,
+    clean_esdr_3508,
+    clean_esdr_5975
 )
 
 default_args_dict = {
@@ -58,6 +70,102 @@ extract_smell_data = PythonOperator(
     trigger_rule='all_success',
 )
 
+clean_esdr_1_data = PythonOperator(
+    task_id='clean_esdr_1_data',
+    python_callable=clean_esdr_1,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_3_data = PythonOperator(
+    task_id='clean_esdr_3_data',
+    python_callable=clean_esdr_3,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_23_data = PythonOperator(
+    task_id='clean_esdr_23_data',
+    python_callable=clean_esdr_23,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_24_data = PythonOperator(
+    task_id='clean_esdr_24_data',
+    python_callable=clean_esdr_24,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_26_data = PythonOperator(
+    task_id='clean_esdr_26_data',
+    python_callable=clean_esdr_26,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_27_data = PythonOperator(
+    task_id='clean_esdr_27_data',
+    python_callable=clean_esdr_27,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_28_data = PythonOperator(
+    task_id='clean_esdr_28_data',
+    python_callable=clean_esdr_28,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_29_data = PythonOperator(
+    task_id='clean_esdr_29_data',
+    python_callable=clean_esdr_29,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_43_data = PythonOperator(
+    task_id='clean_esdr_43_data',
+    python_callable=clean_esdr_43,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_3506_data = PythonOperator(
+    task_id='clean_esdr_3506_data',
+    python_callable=clean_esdr_3506,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_3508_data = PythonOperator(
+    task_id='clean_esdr_3508_data',
+    python_callable=clean_esdr_3508,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
+clean_esdr_5975_data = PythonOperator(
+    task_id='clean_esdr_5975_data',
+    python_callable=clean_esdr_5975,
+    dag=dag,
+    depends_on_past=False,
+    trigger_rule='all_success',
+)
+
 end = DummyOperator(
     task_id='end', 
     dag=dag,
@@ -65,4 +173,4 @@ end = DummyOperator(
 )
 
 
-start  >> extract_esdr_data >> extract_smell_data >> end
+start  >> extract_esdr_data >> extract_smell_data >> [clean_esdr_1_data,clean_esdr_3_data,clean_esdr_23_data,clean_esdr_24_data,clean_esdr_26_data,clean_esdr_27_data,clean_esdr_28_data,clean_esdr_29_data,clean_esdr_43_data,clean_esdr_3506_data,clean_esdr_3508_data,clean_esdr_5975_data] >> end
